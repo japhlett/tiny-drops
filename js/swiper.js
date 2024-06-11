de62c375c9ab1b9797bdf49a99f191c8e03fe02c
japhlett <jatakyiwaa001@st.ug.edu.gh>
2024-06-11 16:39:51 +0000

    Create nav section and slider

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+
+//   Initialize slider
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
